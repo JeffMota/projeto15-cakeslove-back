1363f46a23e4dd6a8f32f0a740d49a244325f91b
JeffMota <jmota906@gmail.com>
2023-01-25 20:29:54 -0300

    feat: Listar todos os produtos

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
-import authRouter from "./routes/AuthRoutes.js"
+import authRouter from "../routes/AuthRoutes.js"
+import productRouter from '../routes/ProductsRoutes.js';
+import dotenv from 'dotenv'
 dotenv.config();
 
 const server = express()
 server.use(express.json())
 server.use(cors())
 
-server.use([authRouter])
+server.use([authRouter, productRouter])
 const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => console.log(`Server running in port ${PORT}`))
