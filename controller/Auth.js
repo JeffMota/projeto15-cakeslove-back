e7d317ce5e937bef16b7cb2470cd015bce5d18da
JeffMota <jmota906@gmail.com>
2023-01-27 19:33:54 -0300

    fix: Duplicidade de token no login

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'
 import { v4 as uuidV4 } from 'uuid'
 import db from '../config/database.js'
-
+import { ObjectId } from 'mongodb'
 
 
 export async function signUp(req, res) {
@@ -10,12 +10,12 @@ export async function signUp(req, res) {
     const passwordHashed = bcrypt.hashSync(user.password, 10)
 
     try {
-        
-        await db.collection("users").insertOne({...user, password: passwordHashed })
+
+        await db.collection("users").insertOne({ ...user, password: passwordHashed })
 
         res.status(201).send("usuário cadastrado")
 
-    } catch(error) {
+    } catch (error) {
         res.status(500).send(error.message)
     }
 }
@@ -25,9 +25,29 @@ export async function signIn(req, res) {
     const user = res.locals.user
 
     try {
+
+        const alreadyExist = await db.collection('sessions').findOne({ user: ObjectId(user._id) })
+
+        if (alreadyExist) {
+            const token = uuidV4();
+
+            await db.collection('sessions').updateOne(
+                {
+                    _id: ObjectId(user._id)
+                },
+                {
+                    $set: {
+                        user: user._id,
+                        token
+                    }
+                }
+            )
+            return res.status(200).send({ token })
+        }
+
         const token = uuidV4();
 
-        const teste = await db.collection("sessions").insertOne({ user: user._id, token})
+        const teste = await db.collection("sessions").insertOne({ user: user._id, token })
 
         return res.status(200).send({ token })
 
